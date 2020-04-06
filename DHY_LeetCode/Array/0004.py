074580872bfd184b4a63d5d5d6314b69a0b6cb4d
dhy-git <dhy302@126.com>
2020-04-06 11:09:12 +0800

    confrim

--- DHY_LeetCode/Array/0004.py
+++ DHY_LeetCode/Array/0004.py
@@ -23,19 +23,22 @@
 
 '''
 
-'''
-功能： 递归实现寻找有序数组A[i]~A[n]、B[j]~B[m]中第x小的数，并返回其下标
-'''
-#本地编译加入 Python3 类型注释模块
+
+#本地编译加入模块
 from typing import List
+import sys
 
 #LeetCode 提交代码：
+# @lc code=start
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
         m = len(nums1)
         n = len(nums2)
         return (self.find_index_x(nums1, nums2, 0, 0, int((m+n+1)/2)) + self.find_index_x(nums1, nums2, 0, 0, int((m+n+2)/2)))/2
 
+    '''
+    功能： 递归实现寻找有序数组A[i]~A[n]、B[j]~B[m]中第x小的数，并返回其下标
+    '''
     def find_index_x(self, A, B, i, j, x):
         #边界条件
         if(i > len(A)-1):
@@ -46,15 +49,15 @@ class Solution:
             return (min(A[i],B[j]))
         
         #递归
-        index_midA = i+int(x/2)-1
-        index_midB = j+int(x/2)-1
-        new_x = x - int(x/2)
-        if(A[index_midA] < B[index_midB]):
+        val_midA = A[i+int(x/2)-1] if(i+int(x/2)-1 < len(A))  else sys.maxsize
+        val_midB = B[j+int(x/2)-1] if(j+int(x/2)-1 < len(B))  else sys.maxsize
+        if( val_midA < val_midB):
             #print("cutA  %d %d"%(i, j))
-            return self.find_index_x(A,B,index_midA+1,j,new_x)
+            return self.find_index_x(A,B, i+int(x/2), j, x-int(x/2))
         else:
             #print("cutB  %d %d"%(i, j))
-            return self.find_index_x(A,B,i,index_midB+1,new_x)
+            return self.find_index_x(A,B, i, j+int(x/2), x-int(x/2))
+# @lc code=end
 
 #本地测试代码：
 nums1 = [10, 20, 30, 40]
@@ -62,3 +65,11 @@ nums2 = [50, 60, 70, 80]
 sol = Solution()
 ans = sol.findMedianSortedArrays(nums1, nums2)
 print(ans)
+
+
+'''
+Accepted
+2085/2085 cases passed (84 ms)
+Your runtime beats 55.49 % of python3 submissions
+Your memory usage beats 5.61 % of python3 submissions (13.8 MB)
+'''
